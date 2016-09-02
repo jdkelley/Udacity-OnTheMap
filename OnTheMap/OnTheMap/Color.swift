65b1c1a29e0d53d29df0d086a72b210e569d4ca4
Joshua Kelley <jdkelley@samaritan.org>
2016-09-01 23:54:12 -0400

    Posting a new pin functionality UI completed.

--- OnTheMap/OnTheMap/Color.swift
+++ OnTheMap/OnTheMap/Color.swift
@@ -1,5 +1,5 @@
 //
-//  Color.swift
+//  UIColor.swift
 //  OnTheMap
 //
 //  Created by Joshua Kelley on 7/25/16.
@@ -8,13 +8,25 @@
 
 import UIKit
 
-struct Colors {
+// Colors defined for On The Map.
+extension UIColor {
+    
+    /// Various Udacity Oranges.
     struct UOrange {
         static let light = UIColor(red: 253.0 / 255.0, green: 134.0 / 255.0, blue: 14.0 / 255.0, alpha: 1.0)
         static let normal = UIColor(red: 253.0 / 255.0, green: 126.0 / 255.0, blue: 13.0 / 255.0, alpha: 1.0)
         static let dark = UIColor(red: 253.0 / 255.0, green: 88.0 / 255.0, blue: 6.0 / 255.0, alpha: 1.0)
     }
+    
+    /// The Color of the Login Button on the LoginViewController.
     static let buttonRed = UIColor(red: 238.0 / 255.0, green: 62.0 / 255.0, blue: 5.0 / 255.0, alpha: 1.0)
     
+    /// The blue on the add pin screen.
     static let postPinBlue = UIColor(red: 63.0 / 255.0, green: 116.0 / 255.0, blue: 167.0 / 255.0, alpha: 1.0)
+    
+    /// The color of the gray top bar on the add pin screen.
+    static let barGray = UIColor(red: 217.0 / 255.0, green: 217.0 / 255.0, blue: 213.0 / 255.0, alpha: 0.4)
+    
+    /// Placeholder text color for textfields on the add pin screen.
+    static let placeholderWhite = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.75)
 }
--- OnTheMap/OnTheMap/PostViewController.swift
+++ OnTheMap/OnTheMap/PostViewController.swift
@@ -7,10 +7,21 @@
 //
 
 import UIKit
+import MapKit
 
 class PostViewController: UIViewController {
+    
+    // MARK: - Properties
+    var location = ""
+    var url = ""
+
+    
+    // MARK: Outlets
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var urlTextField: UITextField!
 
     @IBOutlet weak var findButton: UIButton!
+    
     @IBOutlet weak var cancelButton: UIButton!
     
     @IBOutlet weak var topLabel: UILabel!
@@ -21,15 +32,142 @@ class PostViewController: UIViewController {
     @IBOutlet weak var midView: UIView!
     @IBOutlet weak var buttonBar: UIView!
     
+    @IBOutlet weak var enterLocation: UITextField! { didSet { enterLocation.delegate = self } }
+    
+    @IBOutlet weak var mapView: MKMapView! { didSet { mapView.delegate = self } }
+    
+    // MARK: Actions
+    
+    @IBAction func cancelled(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
+    }
+    
+    @IBAction func submitButtonPressed(sender: AnyObject) {
+        if locationTextField != nil {
+            findOnTheMap() 
+        }
+    }
+    
+    // MARK: View Controller Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         cancelButton.layer.cornerRadius = 5.0
+        findButton.layer.cornerRadius = 5.0
+        
+        
+        urlTextField.hidden = true
+        setTextFieldPlaceholders()
+        
+    }
+
+    // MARK: - Custom Methods
+    
+    private func findOnTheMap() {
+        guard locationTextField != nil else {
+            return
+        }
+        
+        guard let text = locationTextField.text else {
+            return
+        }
+        
+        location = text
+        changeInputStates()
     }
     
+    private func submit() {
+        guard urlTextField != nil else {
+            return
+        }
+        
+        guard let text = urlTextField.text else {
+            return
+        }
+        
+        url = text
+        
+        
+        guard postPin() else {
+            // raise error box
+            return
+        }
+        
+        urlTextField.removeFromSuperview()
+        
+        
+        navigationController?.popViewControllerAnimated(true)
+    }
     
+    private func postPin() -> Bool {
+        // post to Parse
+        
+        return false
+    }
     
+    // MARK: UI Methods
     
+    private func changeInputStates() {
+        findButton.setTitle("Submit", forState: .Normal)
+        //let pointA = self.midView.frame.origin.y
+        
+        UIView.animateWithDuration(0.4, delay: 0.0, options: .BeginFromCurrentState, animations: {
+            self.buttonBar.backgroundColor = Colors.barGray
+            self.midView.transform = CGAffineTransformMakeTranslation(0, -self.midView.frame.height)
+            self.bottomLabel.alpha = 0.0
+            self.midLabel.alpha = 0.0
+            self.topLabel.alpha = 0.0
+            self.urlTextField.hidden = false
+            self.cancelButton.setTitleColor(.whiteColor(), forState: .Normal)
+        }) { (success) in
+            
+            // Remove Location TextField
+            self.locationTextField.removeFromSuperview()
+            
+            self.topBar.backgroundColor = Colors.postPinBlue
+            
+            // Remove MidView after finish translating
+            self.midView.removeFromSuperview()
+            
+            // Remove "where are you studying" labels
+            self.bottomLabel.removeFromSuperview()
+            self.midLabel.removeFromSuperview()
+            self.topLabel.removeFromSuperview()
+        }
+    }
+    
+    private func setTextFieldPlaceholders() {
+        urlTextField.attributedPlaceholder = NSAttributedString(string: UIText.EnterLinkPlaceholder, attributes: [ NSForegroundColorAttributeName: Colors.placeholderWhite ])
+        locationTextField.attributedPlaceholder = NSAttributedString(string: UIText.EnterLocationPlaceholder, attributes: [ NSForegroundColorAttributeName: Colors.placeholderWhite ])
+    }
+}
+
+// MARK: - UI Text Field Delegate
+
+extension PostViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if textField == locationTextField {
+            findOnTheMap()
+            return true
+        }
+        if textField == urlTextField {
+            submit()
+            return true
+        }
+        return true
+    }
+    
+    // When you touch off the keyboard, dismiss keyboard
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
+
+// MARK: - MK Map View Delegate
+
+extension PostViewController : MKMapViewDelegate {
     
     
 }
